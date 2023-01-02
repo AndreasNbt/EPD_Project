6a367b30b5fc56d10061ea390d18cdf7867f8bf4
AndreasNbt <andreasn@csd.auth.gr>
2023-01-02 02:20:51 +0200

    Icons now appear on every page, added min-width to login-container

--- index.php
+++ index.php
@@ -15,30 +15,28 @@
             <h1> Συνδεθείτε στη σελίδα </h1> 
         </div>
         
-            <div class="content"> 
-                <div class="login-container flex-column teal big-border center">  
-                    <h2 class="teal mb">Συμπληρώστε τα στοιχεία σας</h2>
-                    <div class="flex-column inputs-container mt">
-                        <form action="PHP_Back_End/loginb.php" method="post">
-                            <div class="flex-column">
-                                <label for="email">Email</label>
-                                <input id="email" type="text" class="input-field announcement-field thin-border" name="email" required>
-                            </div>
-                            <br>
-                            <div class="flex-column">
-                                <label for="password">Κωδικός</label>
-                                <input id="password" type="password" class="input-field announcement-field thin-border" name="password" required>
-                            </div>
-                            <br>
-                           
-                            <button class="sign-btn thin-border teal" type="submit">Σύνδεση</button>
-                        </form>
-                    </div>
+        <div class="content"> 
+            <div class="login-container flex-column teal big-border center">  
+                <h2 class="teal mb">Συμπληρώστε τα στοιχεία σας</h2>
+                <div class="flex-column inputs-container mt">
+                    <form action="PHP_Back_End/loginb.php" method="post">
+                        <div class="flex-column">
+                            <label for="email">Email</label>
+                            <input id="email" type="text" class="input-field announcement-field thin-border" name="email" required>
+                        </div>
                         <br>
-                        
-                    
+                        <div class="flex-column">
+                            <label for="password">Κωδικός</label>
+                            <input id="password" type="password" class="input-field announcement-field thin-border" name="password" required>
+                        </div>
+                        <br>
+                           
+                        <button class="sign-btn thin-border teal" type="submit">Σύνδεση</button>
+                    </form>
                 </div>
+                <br>
             </div>
+        </div>
     </div>
     
 
