4efea87f88c7c7f17e9b029a5b83cce8f2489229
AndreasNbt <andreasn@csd.auth.gr>
2023-01-03 19:03:04 +0200

    Finished doc

--- PHP_Back_End/db_connection.php
+++ PHP_Back_End/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+$sname = "webpagesdb.it.auth.gr:3306";
+$uname = "andreasn";
+$password = "29122001a";
 $db_name = "student3699partB";
 
 $con = mysqli_connect($sname, $uname, $password, $db_name);
